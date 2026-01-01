f9a54fd6547edef4a0619b0d8321bca32375f3e4
Louis Somers <mlsomers@users.noreply.github.com>
2026-01-01 22:22:10 +0100

    Added ASP.Net formatter (untested)

---
+++ LsMsgPackFormatters/LsMsgPackOutputFormatter.cs
@@ -0,0 +1,34 @@
+﻿using LsMsgPack;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using System.Threading.Tasks;
+
+namespace LsMsgPackFormatters
+{
+  public class LsMsgPackOutputFormatter : OutputFormatter
+  {
+    private MsgPackSettings Settings;
+
+    public LsMsgPackOutputFormatter(MsgPackSettings settings)
+    {
+      Settings = settings;
+    }
+
+    public LsMsgPackOutputFormatter() { 
+      SupportedMediaTypes.Add(new Microsoft.Net.Http.Headers.MediaTypeHeaderValue("application/msgpack"));
+      SupportedMediaTypes.Add(new Microsoft.Net.Http.Headers.MediaTypeHeaderValue("application/x-msgpack"));
+      SupportedMediaTypes.Add(new Microsoft.Net.Http.Headers.MediaTypeHeaderValue("application/x-lsmsgpack"));
+    }
+
+    protected override bool CanWriteType(Type type)
+    {
+      return true; // Optimistic :-)
+    }
+
+    public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+    {
+      MsgPackSerializer.Serialize(context.Object, context.HttpContext.Response.Body, Settings);
+      return Task.CompletedTask;
+    }
+  }
+}
